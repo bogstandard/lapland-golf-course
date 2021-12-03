6eaee5333ee9dfb14f976a18c4081406a38f53ea
Eric <eric.daddio@gmail.com>
2021-12-03 20:43:26 +0000

    Feat: very silly daily advent box

--- _js/app.js
+++ _js/app.js
@@ -38,3 +38,54 @@ const app = new Vue({
   components: { Golf },
 });
 
+/**
+ * Goofy advent calendar
+ */
+  const advent = document.querySelector('.advent');
+  const backdoor = advent.querySelector('.backDoor');
+  const door = advent.querySelector('.door');
+
+  advent.addEventListener('click', () => {
+    advent.classList.toggle('doorOpen');
+  });
+
+  const images = [
+    '1.png',
+    '2.png',
+    '3.png',
+    '4.png',
+    '5.png',
+    '6.pn',
+    '7.png',
+    '8.jpg',
+    '9.jpg',
+    '10.png',
+    '11.jpg',
+    '12.jpeg',
+    '13.jpg',
+    '14.jpg',
+    '15.jpg',
+    '16.jpg',
+    '17.png',
+    '18.jpeg',
+    '19.jpg',
+    '20.gif',
+    '21.png',
+    '22.jpg',
+    '23.jpg',
+    '24.jpg',
+    '25.png',
+  ];
+
+  const month = new Date().getMonth();
+  let day = new Date().getDate();
+  if(month == 11 && day >= 1 && day <= 25) {
+    // ..
+  } else {
+    day = 25;
+  }
+
+  door.textContent = day;
+  backdoor.style.backgroundImage = `url(/assets/images/advents/${images[day-1]})`;
+
+
