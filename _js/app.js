1adda3c01836dfebe5b5a6d98538755893f252de
Eric <eric.daddio@gmail.com>
2021-12-03 00:12:58 +0000

    Feat: Comfy Mode!
    Considering making this the default..

--- _js/app.js
+++ _js/app.js
@@ -4,9 +4,37 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 import Golf from './components/Golf.vue';
 
+
+/**
+ * Theme toggle!!
+ */
+const changeTheme = (theme) => {
+    document.body.setAttribute("data-theme", theme)
+    localStorage.setItem("data-theme", theme)
+}
+
+// initial
+let theme = localStorage.getItem('data-theme') || 'light';
+changeTheme(theme);
+
+// checkbox
+const checkbox = document.getElementById("switch");
+checkbox.addEventListener('change', () => {
+    let theme = localStorage.getItem('data-theme') || 'light';
+    if (theme ==='comfy'){
+        changeTheme('light')
+    } else {
+        changeTheme('comfy')
+    }
+});
+
+/**
+ * Vue!!
+ */
 Vue.use(VueSweetalert2);
 
 const app = new Vue({
   el: '#js-golf',
   components: { Golf },
 });
+
