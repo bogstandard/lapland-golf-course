30bf1953f70f387d5c6fb92b041bd47c1e82ee98
Eric <eric.daddio@gmail.com>
2022-10-07 15:16:15 +0100

    set comfy as default theme

--- _js/app.js
+++ _js/app.js
@@ -4,28 +4,27 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 import Golf from './components/Golf.vue';
 
-
 /**
  * Theme toggle!!
  */
 const changeTheme = (theme) => {
-    document.body.setAttribute("data-theme", theme)
-    localStorage.setItem("data-theme", theme)
-}
+  document.body.setAttribute('data-theme', theme);
+  localStorage.setItem('data-theme', theme);
+};
 
 // initial
-let theme = localStorage.getItem('data-theme') || 'light';
+let theme = localStorage.getItem('data-theme') || 'comfy';
 changeTheme(theme);
 
 // checkbox
-const checkbox = document.getElementById("switch");
+const checkbox = document.getElementById('switch');
 checkbox.addEventListener('change', () => {
-    let theme = localStorage.getItem('data-theme') || 'light';
-    if (theme ==='comfy'){
-        changeTheme('light')
-    } else {
-        changeTheme('comfy')
-    }
+  let theme = localStorage.getItem('data-theme') || 'comfy';
+  if (theme === 'comfy') {
+    changeTheme('light');
+  } else {
+    changeTheme('comfy');
+  }
 });
 
 /**
@@ -41,29 +40,27 @@ const app = new Vue({
 /**
  * Goofy advent calendar
  */
-  const advent = document.querySelector('.advent');
-  const backdoor = advent.querySelector('.backDoor');
-  const door = advent.querySelector('.door');
+const advent = document.querySelector('.advent');
+const backdoor = advent.querySelector('.backDoor');
+const door = advent.querySelector('.door');
 
-  advent.addEventListener('click', () => {
-    advent.classList.toggle('doorOpen');
-  });
+advent.addEventListener('click', () => {
+  advent.classList.toggle('doorOpen');
+});
 
-  const month = new Date().getMonth();
-  let day = new Date().getDate();
-  if(month == 11 && day >= 1 && day <= 25) {
-    // ..
-  } else {
-    day = 25;
-  }
+const month = new Date().getMonth();
+let day = new Date().getDate();
+if (month == 11 && day >= 1 && day <= 25) {
+  // ..
+} else {
+  day = 25;
+}
 
-  const img = new Image();
-  img.onload = () => {
-    advent.classList.add('is-visible');
-    door.textContent = day;
-    backdoor.style.backgroundImage = `url("./assets/images/advents/${day}.png")`;
-  };
-  img.onerror = ()=>{};
-  img.src = `./assets/images/advents/${day}.png`;
-
-
+const img = new Image();
+img.onload = () => {
+  advent.classList.add('is-visible');
+  door.textContent = day;
+  backdoor.style.backgroundImage = `url("./assets/images/advents/${day}.png")`;
+};
+img.onerror = () => {};
+img.src = `./assets/images/advents/${day}.png`;
